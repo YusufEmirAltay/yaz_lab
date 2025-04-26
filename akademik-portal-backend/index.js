8a6d449e966e40d320859c39ea4183747cd86c4d
YusufEmirAltay <tucyusufemir@gmail.com>
2025-04-27 00:59:14 +0300

    env dosyası

--- akademik-portal-backend/index.js
+++ akademik-portal-backend/index.js
@@ -201,8 +201,8 @@ app.post('/api/basvuru', async (req, res) => {
 
 // AWS bağlantı ayarları
 AWS.config.update({
-  accessKeyId: process.env.AKIAQLVQQO7RXNB7YWEW,
-  secretAccessKey: process.env.qXvyFlF5kz/Ce3D8lnAiag+E5dkhMEaLbMeFUk9L,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: 'eu-central-1' // Frankfurt seçtiysen
 });
 
